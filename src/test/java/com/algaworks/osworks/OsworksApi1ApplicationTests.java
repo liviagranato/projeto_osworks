e925ba5b60b11d285871673518ce80baa54d2e00
liviagranato <liviagranato@hotmail.com>
2020-04-24 01:37:56 -0300

    subindo arquivos do projeto

---
+++ src/test/java/com/algaworks/osworks/OsworksApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsworksApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
